0e817388f24f99505535d502cea83da4e6dc9178
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2025-05-28 22:48:25 -0300

    commit v0.2

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.post("/users", (req, res) => {
+  console.log(req);
+  
+  res.send("Post OK...")
+})
+
 app.get("/users", (req, res) => {
   res.send("Working...");
 });
