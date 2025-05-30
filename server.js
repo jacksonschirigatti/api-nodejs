045dcc4478d3e93c78f4e77ff5106721b80b3836
Jackson L F Schirigatti <jacksonschirigatti.fag@gmail.com>
2025-05-30 14:39:57 -0300

    commit err

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 import express from "express";
 
+import { PrismaClient } from "app/generated/prisma/client";
+const prisma = new PrismaClient();
+// use `prisma` in your application to read and write data in your DB
+
 const app = express();
 app.use(express.json());
 
 const users = [];
 
 app.post("/users", (req, res) => {
-
-  users.push(req.body)
+  users.push(req.body);
 
   res.send("Post OK...");
 });
 
 app.get("/users", (req, res) => {
-  res.json(users)
+  res.json(users);
 });
 
 app.listen("3000");
